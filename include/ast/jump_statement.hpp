878de0c3cbfcb97995742d495ca614db81ab5a91
Alek Ratzloff <alekratz@gmail.com>
2016-02-04 17:56:12 -0500

    Forgot to include that actual header

---
+++ include/ast/jump_statement.hpp
@@ -0,0 +1,37 @@
+#ifndef AST_JUMP_STATEMENT_HPP
+#define AST_JUMP_STATEMENT_HPP
+
+#include <memory>
+
+namespace ast
+{
+
+class jump_statement;
+typedef std::shared_ptr<jump_statement> jump_statement_p;
+
+enum class jump_statement_type
+{
+    BREAK,
+    CONTINUE,
+    RETURN,
+};
+
+class jump_statement
+    : public node
+{ 
+public:
+    jump_statement(jump_statement_type type) : type(type) { }
+    virtual ~jump_statement() = default;
+
+    jump_statement_type type;
+
+public:
+    virtual void accept(visitor_p guest);
+    virtual void children_accept(visitor_p guest) { }
+    virtual void traverse_top_down(visitor_p guest) { accept(guest); }
+    virtual void traverse_bottom_up(visitor_p guest) { accept(guest); }
+};
+
+} /* namespace ast */
+
+#endif /* AST_ARRAY_COL_LIST_HPP */

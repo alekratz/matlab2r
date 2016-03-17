b41ed775687ca864ca5e69bb030781479871bebd
Alek Ratzloff <alekratz@gmail.com>
2016-03-17 16:05:59 -0400

    Add sanity_check_visitor.hpp

---
+++ include/visitor/sanity_check_visitor.hpp
@@ -0,0 +1,43 @@
+#ifndef VISITOR_SANITY_CHECK_VISITOR_HPP
+#define VISITOR_SANITY_CHECK_VISITOR_HPP
+
+#ifndef VISITOR_HPP
+#error This file should not be included standalone. Please include visitor.hpp instead.
+#endif
+
+enum class sanity_check_status
+{
+    NOT_STARTED,
+    IN_PROGRESS,
+    SUCCESS,
+    ERROR,
+};
+
+/**
+ * This visitor ensures that the MATLAB code plugged in is sane. This includes:
+ *     * Matrix dimensions
+ */
+class sanity_check_visitor
+    : private visitor
+{
+public:
+    sanity_check_visitor();
+    virtual ~sanity_check_visitor() = default;
+
+public:
+    void start(ast::statement_list* ast);
+    
+    const sanity_check_status get_status() const { return status; }
+    const bool is_successful() const 
+        { return status == sanity_check_status::SUCCESS; }
+    void reset() { status = sanity_check_status::NOT_STARTED; }
+    
+private:
+    virtual void visit(ast::primary_expression*);
+    
+private:
+    sanity_check_status status;
+    friend class ast::node;
+};
+
+#endif

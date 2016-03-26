d826c8b1f1b966b3db5c8a8ca0829c121915b9e7
Alek Ratzloff <alekratz@gmail.com>
2016-03-26 12:29:59 -0400

    Reimplement logspace function translation using the new method

--- src/visitor/function_name_visitor.cpp
+++ src/visitor/function_name_visitor.cpp
@@ -39,29 +39,27 @@ void function_name_visitor::visit(ast::expression* expr)
         if(mapper != fname_map.end()) {
             auto array_index = (*qual_id_item->array_index_list->begin());
             array_index->type = array_index_type::FUNCALL;
-            fname_map[qual_id_item->identifier](qual_id_item.get());
+            fname_map[qual_id_item->identifier](expr, qual_id_item.get());
         }
     }
 }
 
 void function_name_visitor::init_fname_map()
 {
-    fname_map["linspace"] = [](ast::qualified_id_item* item) {
+    fname_map["linspace"] = [](expression* expr, qualified_id_item* item) {
         auto args = (*item->array_index_list->begin())->index_expression_list;
         if(args->size() != 3) { 
             cerr << "error: unable to convert linspace function to seq function, "
                     "incorrect number of arguments (got " << args->size() << " vs 3). skipping" << endl;
             return;
         }
-        item->identifier = string("seq");
+        item->identifier = "seq";
         auto rhs = args->items[2]->expr; // TODO : colon operator
         args->items[2]->expr = nullptr;
         args->items[2]->named_expr = make_shared<generator::funcall_arg_assign>("len", rhs);
     };
 
-    fname_map["logspace"] = [](ast::qualified_id_item* item) {
-        // HACK : setting the expression section of the qualified ID item doesn't seem right
-        //        maybe add a parent item or something... I dunno.
+    fname_map["logspace"] = [](expression* expr, qualified_id_item* item) {
         auto args = (*item->array_index_list->begin())->index_expression_list;
         if(args->size() != 3) { 
             cerr << "error: unable to convert logspace function to seq function, "
@@ -69,19 +67,14 @@ void function_name_visitor::init_fname_map()
             return;
         }
         
-        auto seq_qid_item = make_shared<qualified_id_item>("seq");
-        seq_qid_item->array_index_list = item->array_index_list;
+        expr->lhs = expression::build(10.0);
+        expr->op = expression_op::POW;
+
+        item->identifier = "seq";
         auto rhs = args->items[2]->expr;
         args->items[2]->expr = nullptr;
         args->items[2]->named_expr = make_shared<generator::funcall_arg_assign>("len", rhs);
-        item->array_index_list = nullptr;
-        item->identifier = "";
-
-        item->expression = make_shared<expression>(
-            expression::build(10.0),
-            expression_op::POW,
-            expression::build(make_shared<qualified_id>(seq_qid_item)));
-        item->type = qualified_id_item_type::EXPRESSION;
+        expr->rhs = make_shared<expression>(expr->expr);
+        expr->expr = nullptr;
     };
-
 }

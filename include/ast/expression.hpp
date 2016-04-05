6d2c81175cd3c4d8411c875dd04c96dac3261c9b
Alek Ratzloff <alekratz@gmail.com>
2016-04-05 17:15:40 -0400

    Add build_identifier and build_function methods
    Both of which are helper/convenience methods.

--- include/ast/expression.hpp
+++ include/ast/expression.hpp
@@ -62,6 +62,25 @@ public:
     template<typename T>
     static expression_p build(T val)
         { return std::make_shared<expression>(std::make_shared<primary_expression>(val)); }
+    
+    static expression_p build_identifier(cstref val)
+        { return std::make_shared<expression>(std::make_shared<primary_expression>(
+            std::make_shared<qualified_id>(
+            std::make_shared<qualified_id_item>(val)))); }
+
+    static expression_p build_function(cstref fname, std::initializer_list<expression_p> values) {
+        auto result = build_identifier(fname);
+        auto qid_item = (*result->expr->qualified_id->items.begin());  // this is made by the build_identifier function
+        auto    array_idx_list = 
+                qid_item->array_index_list = std::make_shared<array_index_list>();
+        auto idx_expr_list = std::make_shared<index_expression_list>();
+        auto array_idx = std::make_shared<array_index>(idx_expr_list, array_index_type::FUNCALL);
+        array_idx_list->add_front(array_idx);
+        for(auto expr : values) {
+            idx_expr_list->items.push_back(std::make_shared<index_expression>(expr));
+        }
+        return result;
+    }
 };
 
 };
